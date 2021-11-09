26887879c606f0dbe7e9b79f77fba14b95aae9aa
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-11-09 20:56:30 +0100

    reverse a string

---
+++ Open-Lab-04.07/Open-Lab-04.07/StringTools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_04._07
+{
+    public class StringTools
+    {
+        public string Reverse(string original)
+        {
+          string done = Reverse(original);
+            return done;
+        }
+    }
+}
